a907dbd660e1c2d01a878d5cc82562491d01e4de
raheem paxton <raheem.jp@gmail.com>
2021-12-05 22:43:56 -0600

    completed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# big-data-challenge
+# Can you handle big data?
+
+## Purpose
+
+Create DataFrames to match production-ready tables from two big Amazon customer review datasets. Analyze whether reviews from Amazon's Vine program are trustworthy.
---
+++ Schema.sql
@@ -0,0 +1,32 @@
+#Schema
+''''CREATE TABLE review_id_table (
+	review_id INT,
+	customer_id TEXT,
+	product_id TEXT,
+	product_parent TEXT,
+	review_date DATE
+	
+);
+
+CREATE TABLE product (
+	product_id TEXT,
+	product_title TEXT
+);
+
+CREATE TABLE customers (
+	customer_id INT
+	customer_count INT
+
+);
+
+CREATE TABLE vine_table (
+	review_id TEXT,
+	star_rating INT,
+	helpful_votes INT,
+	total_votes INT,
+	vine TEXT
+	
+
+);
+
+'''
